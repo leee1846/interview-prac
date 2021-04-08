d40302f9a3921d3b0a1397130f0d7ddb9fa47d83
leee1846 <leee1846@gmail.com>
2021-04-08 10:34:46 +0900

    style: loginForm 스타일적용

---
+++ src/components/Login/Login.style.js
@@ -0,0 +1,19 @@
+import Styled from 'styled-components';
+
+export const Container = Styled.div`
+  width:100vw;
+  height:100vw;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`;
+
+export const Form = Styled.form`
+  width:50vw;
+  display:flex;
+  flex-direction:column;
+`;
+
+export const Label = Styled.label`
+  margin:15px 0 0 0;
+`;
