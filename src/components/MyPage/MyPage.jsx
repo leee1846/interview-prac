e4c95977e4eb3127bb7e9bc899f714af3e90acef
leee1846 <leee1846@gmail.com>
2021-04-09 12:29:48 +0900

    feature: 로그아웃시 마이페이지 => 로그인페이지로이동

--- src/components/MyPage/MyPage.jsx
+++ src/components/MyPage/MyPage.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import useLogin from './../../hooks/useLogin';
+import { useHistory } from 'react-router-dom';
 
 const MyPage = () => {
+  const history = useHistory();
+  const { data, mutate } = useLogin('login', () => window.login);
+
+  if (!data) {
+    history.push('/login');
+  }
+
   return (
     <div>
       <p>마이페이지</p>
